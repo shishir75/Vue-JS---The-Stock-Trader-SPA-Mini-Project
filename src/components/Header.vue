cea0a9127b032f49b872deebe31d1c57942298f8
shishir75 <shishir.srdr16@gmail.com>
2020-04-25 00:38:26 +0600

    Adding a Header & Navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,35 @@
 <template>
-    <h1>The Header Components</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <router-link to="/portfolio" class="nav-item" active-class="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
+                <router-link to="/stocks" class="nav-item" active-class="active" tag="li"><a class="nav-link">Stocks</a></router-link>
+            </ul>
+            <ul class="navbar-nav my-2 my-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">End Day</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a
+                       class="nav-link dropdown-toggle"
+                       href="#"
+                       id="navbarDropdown"
+                       role="button"
+                       data-toggle="dropdown"
+                       aria-haspopup="true"
+                       aria-expanded="false">Save & Load
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Save Data</a>
+                        <a class="dropdown-item" href="#">Load Data</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -9,5 +39,8 @@
 </script>
 
 <style scoped>
-
+    .active {
+        color: red !important;
+        background-color: gray;
+    }
 </style>
