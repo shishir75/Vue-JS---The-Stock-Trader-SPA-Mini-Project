b7678c7239a8acb8e67e93e1aee0d7515a4abfd6
shishir75 <shishir.srdr16@gmail.com>
2020-04-25 02:25:23 +0600

    Adding a Buy Button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="col-md-6">
         <div class="card mt-3">
-            <h5 class="card-header">NAME <small>(Price: PRICE)</small></h5>
+            <h5 class="card-header">{{ stock.name }} <small>(Price: {{ stock.price }})</small></h5>
             <div class="card-body">
                 <div style="width: 85%">
-                    <input type="number" class="form-control" placeholder="Quantity" >
+                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
                 </div>
                 <div class="float-right" style="margin-top: -38px;">
-                    <button class="btn btn-success ml-2">Buy</button>
+                    <button class="btn btn-success ml-2"
+                            @click="buyStock"
+                            :disabled="quantity <= 0"
+                    >Buy</button>
                 </div>
             </div>
         </div>
@@ -17,7 +20,24 @@
 
 <script>
     export default {
-        name: "Stock"
+        name: "Stock",
+        props: ['stock'],
+        data() {
+            return {
+              quantity: 0,
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        }
     }
 </script>
 
