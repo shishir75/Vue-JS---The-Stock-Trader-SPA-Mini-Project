a766cffa1de054bd663124bcf87f33430e622e22
shishir75 <shishir.srdr16@gmail.com>
2020-04-24 23:36:06 +0600

    Setup Project Routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import { routes } from "./routes";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes,
+})
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router: router,
+}).$mount('#app');
